15523e39085a8179e5b1bb831029398d3b2f5a38
Milo Eriksson <milo.arvaa@gmail.com>
2025-11-06 19:46:03 +0100

    Refactor Leaderboard class for improved clarity

--- Logic/Leaderboard.cs
+++ Logic/Leaderboard.cs
@@ -1,48 +1,40 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using BrickBreaker.Models;
 using BrickBreaker.Storage;
 
-namespace BrickBreaker.Logic
+namespace BrickBreaker.Logic;
+
+public sealed class Leaderboard
 {
-    public sealed class Leaderboard
+    private readonly LeaderboardStore _store;
+    public Leaderboard(LeaderboardStore store) => _store = store;
+
+    public void Submit(ScoreEntry entry)
     {
-        private readonly LeaderboardStore _store;
+        _store.Add(entry);
+    }
 
-        public Leaderboard(LeaderboardStore store)
-        {
-            _store = store;
-        }
+    // convenience overload
+    public void Submit(string username, int score)
+    {
+        _store.Add(new ScoreEntry(username, score, DateTimeOffset.UtcNow));
+    }
 
-        public void Submit(ScoreEntry entry)
-        {
-            _store.Add(entry);
-        }
+    public List<ScoreEntry> Top(int n)
+    {
+        return _store.ReadAll()
+                     .OrderByDescending(s => s.Score)
+                     .ThenBy(s => s.At)
+                     .Take(n)
+                     .ToList();
+    }
 
-        // Convenience overload
-        public void Submit(string username, int score)
-        {
-            _store.Add(new ScoreEntry(username, score, DateTimeOffset.UtcNow));
-        }
-
-        public List<ScoreEntry> Top(int n)
-        {
-            return _store.ReadAll()
-                .OrderByDescending(s => s.Score)
-                .ThenBy(s => s.At)
-                .Take(n)
-                .ToList();
-        }
-
-        public ScoreEntry? BestFor(string username)
-        {
-            return _store.ReadAll()
-                .Where(s => s.Username.Equals(username, StringComparison.OrdinalIgnoreCase))
-                .OrderByDescending(s => s.Score)
-                .ThenBy(s => s.At)
-                .FirstOrDefault();
-        }
+    public ScoreEntry? BestFor(string username)
+    {
+        return _store.ReadAll()
+                     .Where(s => s.Username.Equals(username, StringComparison.OrdinalIgnoreCase))
+                     .OrderByDescending(s => s.Score)
+                     .ThenBy(s => s.At)
+                     .FirstOrDefault();
     }
 }
 
