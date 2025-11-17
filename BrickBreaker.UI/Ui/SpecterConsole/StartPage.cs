0eae0195612b29d040ac068058ec4acaf917165c
confusedpotatoe <milo.arvaa@gmail.com>
2025-11-17 15:22:03 +0100

    added startpage test

---
+++ BrickBreaker.UI/Ui/SpecterConsole/StartPage.cs
@@ -0,0 +1,17 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrickBreaker.UI.Ui.SpecterConsole
+{
+    public class StartPage
+    {
+        public class Homepage
+        {
+
+        }
+    }
+}
