66e1907229b84298e7b7c3dd694421835c2b4343
tingru00 <grundbergtindra@gmail.com>
2025-11-21 11:24:15 +0100

    Added comments to LoginMenuChoice.cs and a few more to AppState.cs.

--- BrickBreaker.UI/Ui/Enums/AppState.cs
+++ BrickBreaker.UI/Ui/Enums/AppState.cs
@@ -10,9 +10,10 @@ namespace BrickBreaker.UI.Ui.Enums
     //AppState represents the different states the application can be in, out of the following. 
     public enum AppState
     {
-        LoginMenu,
-        GameplayMenu,
-        Playing,
-        Exit
+        LoginMenu, // The application is showing the login menu (start screen).
+        GameplayMenu, // The user is logged in or selected quick play and is viewing gameplay-related options.
+        Playing, // The game itself is running.
+        Exit // The application should close.
     }
 }
+
